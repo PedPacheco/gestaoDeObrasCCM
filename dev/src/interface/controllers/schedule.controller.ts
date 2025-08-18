fe5bf3b169d314f8f8e39c56ed0ea25b4d6c31b2
Pedro Henrique da Silva Pacheco <94586269+PedPacheco@users.noreply.github.com>
2025-08-18 10:39:37 -0300

    Removing console.log

--- dev/src/interface/controllers/schedule.controller.ts
+++ dev/src/interface/controllers/schedule.controller.ts
@@ -204,8 +204,6 @@ export class ScheduleController {
       executionReportData: { ...schedulesData.executionReportData },
     };
 
-    console.log(id);
-
     await this.handleSchedulesUpdateService.update(data, permission);
 
     return {
