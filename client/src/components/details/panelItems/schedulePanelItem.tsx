3725eab1a019e973d6055d42f2202c664a507183
Pedro Henrique Da Silva Pacheco EXTERNO <10009591@edp.com.br>
2025-08-18 10:56:13 -0300

    adjusting the schedule tabel header

--- client/src/components/details/panelItems/schedulePanelItem.tsx
+++ client/src/components/details/panelItems/schedulePanelItem.tsx
@@ -172,7 +172,7 @@ export default function SchedulePanelItem({
         <Table stickyHeader>
           <TableHead>
             <TableRow>
-              <TableCell className="py-1 px-2 text-center text-zinc-700 font-semibold text-lg bg-[#53FF75] border-r border-solid border-zinc-700 min-w-[100px] sticky left-0 z-10"></TableCell>
+              <TableCell className="bg-[#53FF75] border-r border-solid border-zinc-700 min-w-[100px] sticky left-0 z-20"></TableCell>
               {columnConfig.map((col) => (
                 <TableCell
                   key={col.key}
--- client/src/components/layout/Sidebar.tsx
+++ client/src/components/layout/Sidebar.tsx
@@ -40,7 +40,7 @@ export function Sidebar({ open, changeOpen, pathname }: SidebarProps) {
   return (
     <>
       <div
-        className={`fixed top-0 left-0 z-20 h-full bg-[#212E3E] transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 left-0 z-30 h-full bg-[#212E3E] transition-transform duration-300 ease-in-out ${
           open ? "translate-x-0" : "-translate-x-full"
         } w-64`}
       >
