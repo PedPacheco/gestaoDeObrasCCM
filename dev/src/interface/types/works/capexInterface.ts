5649af3f763418b6f74b4b3013f6f1617053c801
Pedro Henrique Da Silva Pacheco EXTERNO <10009591@edp.com.br>
2025-12-26 12:27:13 -0300

    feat(capex): ajustes e melhorias no fluxo de capex

--- dev/src/interface/types/works/capexInterface.ts
+++ dev/src/interface/types/works/capexInterface.ts
@@ -20,6 +20,7 @@ export interface GetAuxiliaryBaseMaterialsInterface {
   ovnota: string;
   ordem_diagrama: string;
   diagrama_rede: string;
+  elemento_pep: string;
   def_proj: string;
   material: string;
   cti: string;
