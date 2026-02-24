311e9e11f971edfef22d2305cf7023507ce710a0
PEDRO HENRIQUE DA SILVA PACHECO COMPET <10009591@edp.com.br>
2026-02-24 08:44:54 -0300

    Changes made to the tables

--- client/src/components/worksComponents/allWorks/allWorksTable.tsx
+++ client/src/components/worksComponents/allWorks/allWorksTable.tsx
@@ -108,7 +108,7 @@ export default function MainAllWorksTable({
                                     </p>
                                   );
                                 }
-                              }
+                              },
                             )}
                           </div>
                         </TableCell>
