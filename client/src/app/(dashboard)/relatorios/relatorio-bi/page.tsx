f808167f496152173d3cb11e5ac59773550d346f
PEDRO HENRIQUE DA SILVA PACHECO COMPET <10009591@edp.com.br>
2026-03-03 09:47:58 -0300

    Added new links of BI

--- client/src/app/(dashboard)/relatorios/relatorio-bi/page.tsx
+++ client/src/app/(dashboard)/relatorios/relatorio-bi/page.tsx
@@ -7,6 +7,11 @@ export const revalidate = 0;
 export default async function BiReports() {
   const exportOptions = [
     {
+      name: "Capex DSPT",
+      path: "obras-multas",
+      visible: false,
+    },
+    {
       name: "Blitz de Segurança",
       path: "https://app.powerbi.com/groups/me/apps/2a7a60bb-a7cd-4376-b59a-662f2e8c478b/reports/853706ab-0389-4dc8-a75b-11ebb358aa03/ReportSectionc68ab510d8e1c026b285?experience=power-bi",
       visible: true,
@@ -21,11 +26,11 @@ export default async function BiReports() {
       path: "https://app.powerbi.com/groups/me/apps/cf6dceda-2355-4078-9a15-d1fd15fc4688/reports/decbfee6-e20b-4fca-bc6d-a0585270e1cb/ReportSection?ctid=bf86fbdb-f8c2-440e-923c-05a60dc2bc9b&experience=power-bi",
       visible: true,
     },
-    // {
-    //   name: "Exportar obras a serem multadas",
-    //   path: "obras-multas",
-    //   visible: false,
-    // },
+    {
+      name: "Gerenciamento SMC",
+      path: "",
+      visible: true,
+    },
   ];
 
   return (
