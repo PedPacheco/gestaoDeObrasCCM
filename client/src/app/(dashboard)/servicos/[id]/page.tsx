d4bc7bbe6b7744f0d919e6969e863d0b6a43a08e
PedPacheco <ppachecobr17@gmail.com>
2026-08-06 10:02:09 -0300

    feat(point-by-point): fetch service points in feasibility and scheduling pages

--- client/src/app/(dashboard)/servicos/[id]/page.tsx
+++ client/src/app/(dashboard)/servicos/[id]/page.tsx
@@ -29,6 +29,7 @@ export default async function ServicosPage({
     serviceTeams,
     scheduledServicesHistory,
     materialsData,
+    points,
   ] = await Promise.all([
     fetchFilters({
       restricao: true,
@@ -74,6 +75,12 @@ export default async function ServicosPage({
       cookieStore.get("token")?.value,
       { cache: "no-store" },
     ),
+    fetchData(
+      `${process.env.NEXT_PUBLIC_API_URL}/servicos/pontos/${id}`,
+      undefined,
+      cookieStore.get("token")?.value,
+      { cache: "no-store" },
+    ),
   ]);
 
   return (
@@ -92,6 +99,7 @@ export default async function ServicosPage({
         idStatusWork={Number(idStatusWorkCookie)}
         idSchedule={Number(formData?.id)}
         statusSchedule={statusSchedule}
+        points={points.data}
       />
     </EmotionCacheProvider>
   );
