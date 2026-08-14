8a6d2018db64d50947dbd468e702ac3998a877b6
PedPacheco <ppachecobr17@gmail.com>
2026-08-14 11:05:16 -0300

    removed console.log

--- dev/src/application/usecases/executionReport.service.ts
+++ dev/src/application/usecases/executionReport.service.ts
@@ -87,8 +87,6 @@ export class ExecutionReportService {
         scheduledFinishTime,
       );
 
-      console.log(executionReport.toPersistenceObject());
-
       await this.executionReportRepository.create(
         executionReport.toPersistenceObject() as Prisma.relatorio_execucaoUncheckedCreateInput,
         tx,
